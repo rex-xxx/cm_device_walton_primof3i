2c885f84cb7d446bc0a41505a1b7a14a06559150
Aniruddha Adhikary <aniruddha@adhikary.net>
2013-12-23 22:20:46 +0600

    mkvendor.sh: Initial commit of primof3i

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := primof3i
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/walton/primof3i/device_primof3i.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := primof3i
+PRODUCT_NAME := cm_primof3i
+PRODUCT_BRAND := walton
+PRODUCT_MODEL := primof3i
+PRODUCT_MANUFACTURER := walton
